dd4d1909ccd356e82476d823b83152ef3b5f434d
hasan-hayder <hasan.heidar2001@gmail.com>
2023-10-29 16:40:51 +0400

    button change

--- src/components/SubscriptionButton.tsx
+++ src/components/SubscriptionButton.tsx
@@ -20,7 +20,7 @@ const SubscriptionButton = ({ isPro }: Props) => {
     }
   };
   return (
-    <Button disabled={loading} onClick={handleSubscription}>
+    <Button disabled={loading} onClick={handleSubscription} variant="outline">
       {isPro ? "Manage Subscription" : "Get Pro"}
     </Button>
   );
